6e3846dec5b4c632db32ff7ab694513ee71cf67d
Blastose <blastoisejay@gmail.com>
2023-08-07 11:50:06 -0700

    Add button to expand/collapse synopsis

--- src/routes/book/[id]/+page.svelte
+++ src/routes/book/[id]/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import { buildBase64ImageUrl } from '$lib/util/util.js';
-	import { IconBook } from '@tabler/icons-svelte';
+	import { IconBook, IconChevronUp } from '@tabler/icons-svelte';
 
 	export let data;
 
+	let expandedSynopsis = false;
+	let parentSynopsisElement: HTMLDivElement;
+	let synopsisElement: HTMLParagraphElement;
+
 	let titleElement: HTMLParagraphElement;
 	let timeout: ReturnType<typeof setTimeout>;
-	let delay = 100;
+	let delay = 96;
 
-	function resizeText(node: HTMLElement) {
+	function resizeTitle(node: HTMLElement) {
 		const parent = document.querySelector('div.title')!;
 		const GAP_HEIGHT = 8;
 		const AUTHOR_TEXT_HEIGHT = 24;
@@ -20,10 +24,19 @@
 		}
 	}
 
+	function resizeSynopsis() {
+		if (expandedSynopsis) {
+			parentSynopsisElement.style.maxHeight = `${synopsisElement.clientHeight}px`;
+		} else {
+			parentSynopsisElement.style.maxHeight = `96px`;
+		}
+	}
+
 	function onWindowResize() {
 		clearTimeout(timeout);
 		timeout = setTimeout(() => {
-			resizeText(titleElement);
+			resizeTitle(titleElement);
+			resizeSynopsis();
 		}, delay);
 	}
 </script>
@@ -32,7 +45,7 @@
 
 <div class="-mt-16 grid-container container-mi">
 	<div
-		style={`background-image:linear-gradient(rgba(255, 255, 255, 0.99), rgba(255, 255, 255, 0.3)), url("${buildBase64ImageUrl(
+		style={`background-image:linear-gradient(rgba(255, 255, 255, 0.99), rgba(255, 255, 255, 0.5)), url("${buildBase64ImageUrl(
 			data.book.cover ?? ''
 		)}");`}
 		class="bg-no-repeat bg-cover -z-10 bg"
@@ -49,7 +62,7 @@
 	<div class="flex flex-col justify-between gap-1 py-2 overflow-hidden title">
 		<p
 			bind:this={titleElement}
-			use:resizeText
+			use:resizeTitle
 			style="font-size: 36px;"
 			class="max-w-3xl text-4xl font-bold"
 		>
@@ -70,21 +83,43 @@
 	<div class="flex flex-col gap-4 content">
 		<div class="flex flex-col gap-2">
 			<p class="text-lg font-bold">Synopsis:</p>
-			<p class="max-w-5xl">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ipsum nulla, pretium vitae
-				velit sollicitudin, facilisis tempus orci. Nulla a sem facilisis, ullamcorper mi sed,
-				vestibulum enim. Cras efficitur rutrum luctus. Maecenas tempus, arcu in lobortis ultricies,
-				massa tellus interdum arcu, nec rhoncus massa est sit amet neque. Quisque volutpat, mauris
-				eget iaculis dignissim, ipsum nibh maximus eros, nec molestie diam arcu sed augue. Aliquam
-				efficitur, tellus ac rutrum congue, nunc nulla elementum sapien, nec laoreet justo metus
-				vitae eros. Pellentesque imperdiet lectus nec ipsum elementum egestas. Quisque interdum
-				ipsum quis elementum malesuada. Morbi vulputate vulputate sapien, a sodales sem faucibus ut.
-				Maecenas ultricies ultricies velit eget ullamcorper. Class aptent taciti sociosqu ad litora
-				torquent per conubia nostra, per inceptos himenaeos. Proin elementum turpis molestie, porta
-				lectus quis, scelerisque risus. Sed sit amet mauris a urna hendrerit vulputate. Pellentesque
-				feugiat varius mauris et tempor. Aliquam tristique massa a magna cursus, quis ultricies leo
-				iaculis. Cras sed justo egestas, volutpat ipsum ut, rhoncus quam.
-			</p>
+			<div
+				bind:this={parentSynopsisElement}
+				class="max-w-5xl overflow-hidden duration-300 max-h-24"
+			>
+				<p bind:this={synopsisElement}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ipsum nulla, pretium
+					vitae velit sollicitudin, facilisis tempus orci. Nulla a sem facilisis, ullamcorper mi
+					sed, vestibulum enim. Cras efficitur rutrum luctus. Maecenas tempus, arcu in lobortis
+					ultricies, massa tellus interdum arcu, nec rhoncus massa est sit amet neque. Quisque
+					volutpat, mauris eget iaculis dignissim, ipsum nibh maximus eros, nec molestie diam arcu
+					sed augue. Aliquam efficitur, tellus ac rutrum congue, nunc nulla elementum sapien, nec
+					laoreet justo metus vitae eros. Pellentesque imperdiet lectus nec ipsum elementum egestas.
+					Quisque interdum ipsum quis elementum malesuada. Morbi vulputate vulputate sapien, a
+					sodales sem faucibus ut. Maecenas ultricies ultricies velit eget ullamcorper. Class aptent
+					taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin
+					elementum turpis molestie, porta lectus quis, scelerisque risus. Sed sit amet mauris a
+					urna hendrerit vulputate. Pellentesque feugiat varius mauris et tempor. Aliquam tristique
+					massa a magna cursus, quis ultricies leo iaculis. Cras sed justo egestas, volutpat ipsum
+					ut, rhoncus quam.
+				</p>
+			</div>
+
+			{#if synopsisElement?.clientHeight > 96}
+				<button
+					class="duration-300 flex items-center justify-center rounded-md hide-text-gradient
+					{expandedSynopsis ? 'mt-0' : '-mt-8'}"
+					on:click={() => {
+						expandedSynopsis = !expandedSynopsis;
+						resizeSynopsis();
+					}}
+					aria-label={expandedSynopsis ? 'Show less' : 'Show more'}
+				>
+					<IconChevronUp
+						class="duration-200 ease-out {expandedSynopsis ? 'rotate-0' : 'rotate-180'}"
+					/>
+				</button>
+			{/if}
 		</div>
 
 		<div>
@@ -142,6 +177,14 @@
 	.description {
 		grid-row: 3;
 		grid-column: 1 / 3;
+	}
+
+	.hide-text-gradient {
+		background: linear-gradient(
+			rgba(0, 0, 0, 0) 0%,
+			rgba(255, 255, 255, 1) 90%,
+			rgba(255, 255, 255, 1) 100%
+		);
 	}
 
 	@media (min-width: 640px) {
