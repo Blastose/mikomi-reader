362799c1fa78ed0d0020a61ad94d568cf0fc6ec2
Blastose <blastoisejay@gmail.com>
2023-08-09 14:00:57 -0700

    Change home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
+	import { buildBase64ImageUrl } from '$lib/util/util.js';
+
+	export let data;
 </script>
 
-<div class="flex flex-col gap-2 container-mi">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col gap-2 py-6 container-mi">
+	<div class="flex flex-col gap-2">
+		<h2 class="text-xl font-bold">Recently read</h2>
+		<div class="flex items-end gap-2">
+			{#each data.books.slice(-4) as book}
+				<a href="/book/{book.book.id}" class="flex-1 flex flex-col max-w-[200px] gap-1">
+					{#if book.cover}
+						<img class="rounded-md shadow-md" src={buildBase64ImageUrl(book.cover)} alt="" />
+					{/if}
+					<div class="flex flex-col">
+						<p class="text-sm sm:text-base font-bold line-clamp-1">{book.book.title}</p>
+						<p class="text-sm sm:text-base text-gray-500">{Math.floor(Math.random() * 100)}%</p>
+					</div>
+				</a>
+			{/each}
+		</div>
+	</div>
 </div>
