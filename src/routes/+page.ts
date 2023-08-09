3b64877a711176f517a869a5bbb4b4a1be443904
Blastose <blastoisejay@gmail.com>
2023-08-09 12:33:10 -0700

    Add caching

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,17 +2,13 @@ import type { PageLoad } from './$types';
 import { getBooks } from '$lib/bindings';
 import { cacheOrRun, setCache } from '$lib/cache/cache';
 
-// TODO - placeholder for testing
-export const load = (async ({ url, params }) => {
+export const load = (async ({ url }) => {
 	const data = cacheOrRun(url.toString(), getBooks);
-	const books = await data;
+
 	const loadedData = await data;
 	setCache(url.toString(), loadedData);
 
-	const book = books.find((e) => e.book.id === params.id);
-	if (!book) {
-		throw Error('');
-	}
-
-	return { book };
+	return {
+		books: data
+	};
 }) satisfies PageLoad;
