d33c8a9bc9626052b30d8ee8c8af6a8d7c2fa445
Blastose <blastoisejay@gmail.com>
2023-09-11 14:04:42 -0700

    Fix completely overlapping check

--- src/lib/components/overlayer/utils.ts
+++ src/lib/components/overlayer/utils.ts
@@ -12,11 +12,12 @@ export function filterCompletelyOverlappingRectangles<T extends Rectangle>(rects
 			const rect1 = rects[i];
 			const rect2 = rects[j];
 
+			// if rect1 is completely inside rect2, it completely overlaps
 			if (
-				rect1.x <= rect2.x &&
-				rect1.y <= rect2.y &&
-				rect1.x + rect1.width >= rect2.x + rect2.width &&
-				rect1.y + rect1.height >= rect2.y + rect2.height
+				rect1.x >= rect2.x &&
+				rect1.y >= rect2.y &&
+				rect1.x + rect1.width <= rect2.x + rect2.width &&
+				rect1.y + rect1.height <= rect2.y + rect2.height
 			) {
 				isOverlapping = true;
 				overlappingIndices.push(i);
