e3c21c81c2e78fd3394669811daf2ed3754add58
Blastose <blastoisejay@gmail.com>
2023-10-07 20:44:54 -0700

    Redirect to url without searchQuery param when searchQuery is empty string

--- src/routes/books/+page.ts
+++ src/routes/books/+page.ts
@@ -1,16 +1,22 @@
 import type { PageLoad } from './$types';
 import { getBooks, getLanguages } from '$lib/bindings';
 import { escapeRegExp } from '$lib/components/reader/search/search';
+import { redirect } from '@sveltejs/kit';
 
 export const load = (async ({ url }) => {
-	let books = await getBooks();
-	const databaseLanguages = await getLanguages();
-
 	console.log(url.pathname + url.search);
 
 	const statuses = url.searchParams.getAll('status');
 	const languages = url.searchParams.getAll('lang');
 	const searchQuery = url.searchParams.get('q');
+
+	if (searchQuery === '') {
+		url.searchParams.delete('q');
+		throw redirect(307, url.pathname + url.search);
+	}
+
+	let books = await getBooks();
+	const databaseLanguages = await getLanguages();
 
 	if (statuses.length > 0) {
 		console.log(statuses);
